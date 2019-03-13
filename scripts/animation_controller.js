9f42a4ba6380c0826fb64ecc9a50e2780d7effe2
PC03\e.melnikova <kmilleraf@gmail.com>
2019-03-13 18:26:37 +0300

    отзеркаливание оптимизированно

--- scripts/animation_controller.js
+++ scripts/animation_controller.js
@@ -2,11 +2,11 @@ class AnimationController {
 
   constructor(animation_config, canvas_id) {
   	if ( animation_config ) {
+
+
 	  	this.width = animation_config.width;
 	  	this.height = animation_config.height;
 	  	this.scale = animation_config.scale;  		
-
-	  	this.animation_object = animation_config.animation_object;
 
 	  	this.canvas_id = canvas_id;
 	  	this.animation = animation_config.animation_object;
@@ -27,8 +27,8 @@ class AnimationController {
 
 		//init stage
 		function handleComplete() {
-	  	var exportRoot = scope.animation_object = new lib.trener();
 	  	// var exportRoot = scope.animation_object = scope.animation;
+	  	var exportRoot = scope.animation_object = new lib.trener();
 	  	console.log('ANIMATION OBJ: ', exportRoot);
 	  	var stage = scope.stage = new createjs.Stage(scope.canvas_id);
 	  	createContainer(stage);
@@ -40,8 +40,6 @@ class AnimationController {
 
 			scope.container.addChild(exportRoot);
 	  	stage.update();
-	  	// console.log(exportRoot.getCurrentLabel())
-  		// exportRoot.gotoAndPlay('Tr_C_Hand_1');
 		}
 
 		function createContainer(stage) {
@@ -69,8 +67,6 @@ class AnimationController {
 
   pauseAnimation() {
   	this.animation_object.tickChildren = false;
-	  	// this.animation_object.gotoAndPlay(5);
-		// this.animation_object.getLabels();
   }
 
   resumeAnimation() {
@@ -81,16 +77,10 @@ class AnimationController {
   	var scale_x = this.container.scaleX;
   	var width = this.container.getBounds().width;
 		var animation_x = this.container.getBounds().x;
-		var result_x = width * 2 + animation_x;
-		if ( scale_x < 0 )
-		{
-	  	this.container.scaleX = 1;
-	  	this.container.x -= result_x;
-
-		} else {
-			this.container.scaleX = -1;
-	  	this.container.x += result_x;
-		}
+		var result_x = width + animation_x;
+		
+		this.container.scaleX = (this.container.scaleX < 0 ) ? 1: -1;
+  	this.container.x = this.container.x + this.container.scaleX * (-1) * result_x;
   }
 
   mirrorY() {
@@ -98,15 +88,9 @@ class AnimationController {
   	var height = this.container.getBounds().height;
 		var animation_y = this.container.getBounds().y;
 		var result_y = height + animation_y;
-		if ( scale_y < 0 )
-		{
-	  	this.container.scaleY = 1;
-	  	this.container.y -= result_y;
 
-		} else {
-			this.container.scaleY = -1;
-	  	this.container.y += result_y;
-		}
+		this.container.scaleY = (this.container.scaleY < 0 ) ? 1: -1;
+  	this.container.y = this.container.y + this.container.scaleY * (-1) * result_y;
   }
 }
     
